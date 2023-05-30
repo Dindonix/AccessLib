ea34ec90f1ed012fbe6536eb7596d658fa5e933f
matthieu <georgematthieu1@gmail.com>
2023-05-30 16:37:09 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@services": path.resolve(__dirname, "src/services"),
-    },
+    alias: {},
   },
 });
