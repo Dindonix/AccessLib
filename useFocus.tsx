07eaf04d09e66b9589922cca9d567e9c7284efbe
matthieu george <83599039+Dindonix@users.noreply.github.com>
2023-05-30 14:49:58 +0200

    Update useFocus.tsx
    added a new parameter and jsDoc

--- useFocus.tsx
+++ useFocus.tsx
@@ -1,21 +1,75 @@
 import { useEffect, useState, RefObject, useCallback } from "react";
 
+const body = document.querySelector("body");
+
 /**
- * My hook useFocus
+ * useFocus is a custom React hook that enables keyboard navigation on a large range of components.
+ *
+ * @param {Array<RefObject<HTMLButtonElement>>} refArray - An array of RefObjects that require an HTMLButtonElement type (mandatory).
+ * @param {number} [initialIndex] - A number representing the initial index of the refArray, providing an option for positioning the initial focus on mount (optional).
+ *
+ * @returns {{
+ *   horizontalFocus: (event: KeyboardEvent) => void,
+ *   verticalFocus: (event: KeyboardEvent) => void,
+ *   activeFocus: any,
+ *   setActiveFocus: React.Dispatch<React.SetStateAction<any>>
+ * }} - Object containing two different functions: horizontalFocus and verticalFocus, along with activeFocus and setActiveFocus state variables.
+ *
+ * @function horizontalFocus - A function that enables left and right arrow key navigation through the refArray.
+ * @param {KeyboardEvent} event - The event object passed from the onKeyDown event.
+ *
+ * @function verticalFocus - A function that enables up and down arrow key navigation through the refArray.
+ * @param {KeyboardEvent} event - The event object passed from the onKeyDown event.
+ *
+ * @example
+ * const ExampleComponent: React.FC = () => {
+ *   const buttonRefs: RefObject<HTMLButtonElement>[] = [
+ *     useRef<HTMLButtonElement>(null),
+ *     useRef<HTMLButtonElement>(null)
+ *   ];
+ *
+ *   const { horizontalFocus } = useFocus(buttonRefs);
+ *
+ *   return (
+ *     <>
+ *       <button
+ *         ref={buttonRefs[0]}
+ *         type="button"
+ *         onKeyDown={(event) => {
+ *           horizontalFocus(event);
+ *         }}
+ *       >
+ *         Button 1
+ *       </button>
+ *
+ *       <button
+ *         ref={buttonRefs[1]}
+ *         type="button"
+ *         onKeyDown={(event) => {
+ *           horizontalFocus(event);
+ *         }}
+ *       >
+ *         Button 2
+ *       </button>
+ *     </>
+ *   );
+ * };
  */
-const useFocus = (refs: RefObject<HTMLElement>[]) => {
+const useFocus = (refs: RefObject<HTMLElement>[], initialIndex?: number) => {
   const [activeFocus, setActiveFocus] = useState<number>(0);
 
   const focusButton = useCallback(
     (index: number) => {
-      refs[index]?.current?.focus();
+      if (document.activeElement !== body || initialIndex !== undefined) {
+        refArray[index]?.current?.focus();
+      }
     },
-    [refs]
+    [initialIndex, refArray]
   );
 
   useEffect(() => {
     focusButton(activeFocus);
-  }, [activeFocus, focusButton, refs]);
+  }, [activeFocus, focusButton]);
 
   const horizontalFocus = (event: KeyboardEvent) => {
     const { key } = event;
